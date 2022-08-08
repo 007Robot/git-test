18507d0a9fce9552e8742e99a9187770b7d206ec
rxcstart <1969370027@qq.com>
2022-08-08 12:27:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author rxcstart
+ * @create 2022-08-08{TIME}
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

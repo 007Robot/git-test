57f10a84c997b86a2843908b68fa8c18e48fd5c4
rxcstart <1969370027@qq.com>
2022-09-02 08:45:30 +0800

    pull test1

---
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author rxcstart
+ * @create 2022-09-02{TIME}
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("三国演义");
+        System.out.println("三国演义");
+    }
+}
